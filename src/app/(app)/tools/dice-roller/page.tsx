871efa98de1b804315af017d66c8804af6d02a60
Luciano Augusto <lucianoabracadabra@gmail.com>
2025-10-16 20:50:29 +0000

    Some stuff aren't correctly colored, like buttons, the background of thi

--- src/app/(app)/tools/dice-roller/page.tsx
+++ src/app/(app)/tools/dice-roller/page.tsx
@@ -3,7 +3,7 @@ import { DiceRoller } from './_components/dice-roller';
 export default function DiceRollerPage() {
   return (
     <div className="w-full max-w-4xl mx-auto flex flex-col gap-6">
-        <h1 className="text-3xl font-headline magical-glow text-center animate-in fade-in-down">Dice Roller</h1>
+        <h1 className="text-3xl font-headline magical-glow animate-in fade-in-down">Dice Roller</h1>
         <div className="animate-in fade-in-up">
             <DiceRoller />
         </div>
