614881e96b23a52d50cdffe4538799fa963b1089
Luciano Augusto <lucianoabracadabra@gmail.com>
2025-10-17 17:14:30 +0000

    Essa DIV está bloqueando efeitos, é possível remover ou a div ou o bloqu

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -14,12 +14,11 @@ export default function AppLayout({ children, activePath, style }: AppLayoutProp
   return (
     <div className="min-h-screen w-full relative overflow-visible">
         <SidebarNav activePath={activePath} />
-        <main className="pl-24 relative min-h-screen p-4 lg:p-6">
-            <div className="page-container-visuals" style={style}>
-                <div className={cn('page-container-content')}>
-                    {children}
-                </div>
-            </div>
+        <main 
+            className={cn('pl-24 relative min-h-screen p-4 lg:p-6 page-container-visuals')}
+            style={style}
+        >
+            {children}
         </main>
     </div>
   );
