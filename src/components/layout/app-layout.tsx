f2543489ca783f64966c89a187dc248e29a72f64
Luciano Augusto <lucianoabracadabra@gmail.com>
2025-10-17 15:11:31 +0000

    Como você está definindo a estrutura, realmente flexbox é a melhor opção

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -12,16 +12,14 @@ type AppLayoutProps = {
 export default function AppLayout({ children, activePath, style }: AppLayoutProps) {
 
   return (
-    <div className="min-h-screen w-full bg-background relative">
-        <div className='relative z-20'>
-            <SidebarNav activePath={activePath} />
-        </div>
-        <main className="pl-24 h-screen relative z-10">
+    <div className="grid grid-cols-[auto_1fr] min-h-screen w-full bg-background relative">
+        <SidebarNav activePath={activePath} />
+        <main className="h-screen relative z-10">
             <div className="h-full w-full">
                 <div 
                     className='page-container-visuals' style={style}
                 >
-                    <div className={cn('page-container-content', 'h-full flex-1 flex flex-col')}>
+                    <div className={cn('page-container-content', 'h-full')}>
                          {children}
                     </div>
                 </div>
