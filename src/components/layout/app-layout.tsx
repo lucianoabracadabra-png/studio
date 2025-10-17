77f75a239ab8531ef8f06f941760bbf35789604a
Luciano Augusto <lucianoabracadabra@gmail.com>
2025-10-17 16:42:14 +0000

    Quero que o plano de fundo dessa página seja o mesmo nas outras páginas

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -12,7 +12,7 @@ type AppLayoutProps = {
 export default function AppLayout({ children, activePath, style }: AppLayoutProps) {
 
   return (
-    <div className="min-h-screen w-full bg-background relative overflow-visible">
+    <div className="min-h-screen w-full relative overflow-visible">
         <SidebarNav activePath={activePath} />
         <main className="pl-24 relative min-h-screen p-4 lg:p-6">
             <div className="page-container-visuals" style={style}>
