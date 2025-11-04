2f808ce54f9633366e87dcbe3266d220fb7cc803
Luciano Augusto <lucianoabracadabra@gmail.com>
2025-11-04 13:10:06 +0000

    Queria os títulos e detalhes em fonte Metamorphous

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['"Roboto Mono"', 'monospace'],
-        headline: ['"Cinzel"', 'serif'],
+        headline: ['"Metamorphous"', 'serif'],
         mono: ['"Roboto Mono"', 'monospace'],
       },
       colors: {
